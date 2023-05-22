b0f29519915887b69a852299192a3e5b12d9524e
AhNyx <zxczxc500@naver.com>
2023-05-22 18:19:16 +0900

    no message

---
+++ pyworks/pyocr/ocr1.py
@@ -0,0 +1,9 @@
+# 이미지 처리 - Pillow
+from PIL import Image
+from pytesseract import pytesseract
+
+pytesseract.tesseract_cmd = "C:/Program Files/Tesseract-OCR/tesseract.exe"
+img = Image.open("source/news.png")
+#img.show()
+text = pytesseract.image_to_string(img, lang="kor")
+print(text)
