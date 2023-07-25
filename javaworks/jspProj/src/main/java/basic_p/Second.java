b98643e5566f1c7a9ef5e3fbf51147b87d9d597b
AhNyx <zxczxc500@naver.com>
2023-07-25 18:25:01 +0900

    2023-07-25

---
+++ javaworks/jspProj/src/main/java/basic_p/Second.java
@@ -0,0 +1,30 @@
+package basic_p;
+
+public class Second {
+
+	int a = 10;
+	int b = 10 + 20;
+	void meth_1() {
+		int a = 10;
+		for(int i = 0; i < 10; i++) {
+			
+		}
+		
+		
+		
+		class AAA{
+			
+		}
+		// 메소드 안에서 메소드 정의 X
+		/*void meth_2() {
+			
+		}*/
+	}
+	 
+	/* for(int i = 0; i<10;i++) {
+	   }*/
+	
+	/*System.out.println(**);*/
+	
+	
+}
